b3042ac3cf90370d3d7c589898637f0f62e43ec4
Aeyma Syahira <241230023@unmuhpnk.ac.id>
2026-04-25 19:50:31 +0700

    second_commit

--- project4_2026/app/Http/Controllers/ProfilController.php
+++ project4_2026/app/Http/Controllers/ProfilController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class HomeController extends Controller    {
+class ProfilController extends Controller
+{
     public function index()
     {
-        return view('home');
+        return view('profil');
+
     }
 }
